8a620c8189b8b11e297bf2925b9e3c3a753f0101
Thierry Delprat <tdelprat@nuxeo.com>
2015-02-17 16:18:16 -0500

    Add sample for Automationn binding + simplify tests

---
+++ src/test/java/org/nuxeo/rendition/cmis/test/TestAutomationRenditions.java
@@ -0,0 +1,119 @@
+package org.nuxeo.rendition.cmis.test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.io.Serializable;
+import java.util.List;
+
+import org.apache.chemistry.opencmis.client.api.CmisObject;
+import org.apache.chemistry.opencmis.client.api.Folder;
+import org.apache.chemistry.opencmis.client.api.ObjectId;
+import org.apache.chemistry.opencmis.client.api.OperationContext;
+import org.apache.chemistry.opencmis.client.api.Rendition;
+import org.apache.chemistry.opencmis.client.api.Session;
+import org.apache.chemistry.opencmis.commons.data.ContentStream;
+import org.apache.commons.io.IOUtils;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mvel2.ast.AssertNode;
+import org.nuxeo.ecm.core.api.Blob;
+import org.nuxeo.ecm.core.api.CoreSession;
+import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.core.api.impl.blob.StringBlob;
+import org.nuxeo.ecm.core.event.EventService;
+import org.nuxeo.ecm.core.opencmis.impl.CmisFeature;
+import org.nuxeo.ecm.core.opencmis.impl.CmisFeatureSessionBrowser;
+import org.nuxeo.ecm.platform.rendition.service.RenditionService;
+import org.nuxeo.runtime.test.runner.Deploy;
+import org.nuxeo.runtime.test.runner.Features;
+import org.nuxeo.runtime.test.runner.FeaturesRunner;
+import org.nuxeo.runtime.test.runner.LocalDeploy;
+import org.nuxeo.runtime.test.runner.RuntimeHarness;
+import org.nuxeo.runtime.transaction.TransactionHelper;
+import org.testng.Assert;
+
+import com.google.inject.Inject;
+
+@RunWith(FeaturesRunner.class)
+@Features({CmisFeature.class, CmisFeatureSessionBrowser.class})
+@Deploy({ "org.nuxeo.ecm.core.cache","org.nuxeo.renditons.cmis.sample"})
+@LocalDeploy({ "org.nuxeo.renditons.cmis.sample:core-type-contrib.xml",
+        "org.nuxeo.renditons.cmis.sample:automation-contrib.xml",
+        "org.nuxeo.renditons.cmis.sample:renditions-automation-test-contrib.xml" })
+/**
+ *
+ * @author tiry
+ *
+ */
+public class TestAutomationRenditions {
+
+
+    @Inject
+    RenditionService rs;
+
+    @Inject
+    CoreSession coreSession;
+
+    @Inject
+    protected RuntimeHarness harness;
+
+    @Inject
+    protected EventService eventService;
+
+    @Inject
+    protected Session session;
+
+    @Test
+    public void verifyAccessAlternateViaRendition() throws Exception {
+
+        DocumentModel doubleBlobDoc = coreSession.createDocumentModel("/", "double", "DocWith2Blobs");
+        doubleBlobDoc.setPropertyValue("dc:title", "Double Blob");
+        Blob blob1 = new StringBlob("PrimaryContent", "text/plain", "UTF-8", "File1.txt");
+        Blob blob2 = new StringBlob("<html>SecondaryContent</html>", "text/html", "UTF-8", "File2.html");
+        doubleBlobDoc.setPropertyValue("file:content", (Serializable)blob1);
+        doubleBlobDoc.setPropertyValue("alternate:secondaryContent", (Serializable)blob2);
+        doubleBlobDoc = coreSession.createDocument(doubleBlobDoc);
+
+        coreSession.save();
+        TransactionHelper.commitOrRollbackTransaction();
+        TransactionHelper.startTransaction();
+
+        // configure CMIS context to fetch the Renditions
+        OperationContext oc = session.createOperationContext();
+        oc.setRenditionFilterString("*");
+
+        // get the Double Blob guy
+        CmisObject dBlobDoc = session.getObjectByPath("/double", oc);
+
+        // Check the main content
+        ContentStream main = session.getContentStream(session.createObjectId(dBlobDoc.getId()));
+        assertEquals("text/plain", main.getMimeType());
+        String mainContent = IOUtils.toString(main.getStream());
+        assertEquals("PrimaryContent", mainContent);
+
+        // get renditions
+        List<Rendition> renditions = dBlobDoc.getRenditions();
+
+        Rendition alternate = null;
+        for (Rendition rendition: renditions) {
+            if ("nuxeo:rendition:alternate".equals(rendition.getStreamId())) {
+                alternate = rendition;
+            }
+        }
+        Assert.assertNotNull(alternate);
+
+        // use rendition to access secondary content
+
+        ContentStream alternateCS = alternate.getContentStream();
+        assertEquals("text/html", alternateCS.getMimeType());
+        String alternateContent = IOUtils.toString(alternateCS.getStream());
+        assertEquals("<html>SecondaryContent</html>", alternateContent);
+
+
+
+
+    }
+
+}
--- src/test/java/org/nuxeo/rendition/cmis/test/TestRenditionWithCMIS.java
+++ src/test/java/org/nuxeo/rendition/cmis/test/TestRenditionWithCMIS.java
@@ -1,6 +1,10 @@
 
 package org.nuxeo.rendition.cmis.test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import java.util.List;
 
 import org.apache.chemistry.opencmis.client.api.Folder;
@@ -9,12 +13,12 @@ import org.apache.chemistry.opencmis.client.api.Rendition;
 import org.apache.chemistry.opencmis.client.api.Session;
 import org.apache.chemistry.opencmis.commons.data.ContentStream;
 import org.apache.commons.io.IOUtils;
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.ecm.core.event.EventService;
 import org.nuxeo.ecm.core.opencmis.impl.CmisFeature;
+import org.nuxeo.ecm.core.opencmis.impl.CmisFeatureSessionBrowser;
 import org.nuxeo.ecm.platform.rendition.service.RenditionService;
 import org.nuxeo.runtime.test.runner.Deploy;
 import org.nuxeo.runtime.test.runner.Features;
@@ -33,11 +37,10 @@ import com.google.inject.Inject;
  *
  */
 @RunWith(FeaturesRunner.class)
-@Features(CmisFeature.class)
+@Features({CmisFeature.class, CmisFeatureSessionBrowser.class})
 @Deploy({ "org.nuxeo.ecm.core.cache","org.nuxeo.renditons.cmis.sample"})
 @LocalDeploy("org.nuxeo.renditons.cmis.sample:renditions-test-contrib.xml")
 public class TestRenditionWithCMIS {
-
 
     @Inject
     RenditionService rs;
@@ -66,15 +69,15 @@ public class TestRenditionWithCMIS {
 
         // get renditions
         List<Rendition> renditions = rootWithRenditions.getRenditions();
-        Assert.assertEquals(1, renditions.size());
+        assertEquals(1, renditions.size());
 
         // try to access rendition and verify that it is not yet available
-        Assert.assertEquals("nuxeo:rendition:iamlazy", renditions.get(0).getStreamId());
+        assertEquals("nuxeo:rendition:iamlazy", renditions.get(0).getStreamId());
         ContentStream stream = renditions.get(0).getContentStream();
-        Assert.assertTrue(renditions.get(0).getContentStream().getMimeType().contains("empty=true"));
+        assertTrue(renditions.get(0).getContentStream().getMimeType().contains("empty=true"));
 
         String content = IOUtils.toString(stream.getStream());
-        Assert.assertEquals(0, content.length());
+        assertEquals(0, content.length());
 
         // wait for async processing on the Nuxeo side
         eventService.waitForAsyncCompletion(2000);
@@ -82,12 +85,12 @@ public class TestRenditionWithCMIS {
         // fetch again the rendition stream
         stream = renditions.get(0).getContentStream();
         // check that is no lo,ger flagged as empty
-        Assert.assertFalse(renditions.get(0).getContentStream().getMimeType().contains("empty=true"));
+        assertFalse(renditions.get(0).getContentStream().getMimeType().contains("empty=true"));
 
         // verify the content of the rendition stream
         content = IOUtils.toString(stream.getStream());
-        Assert.assertTrue(content.length()>0);
-        Assert.assertEquals("I am really lazy", content);
+        assertTrue(content.length()>0);
+        assertEquals("I am really lazy", content);
 
     }
 
